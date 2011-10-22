f81005f47b497621bd27d51c593e3be8194d4975
Stefan Church <stefan.church@googlemail.com>
2011-10-22 17:43:11 +0100

    Initial Commit

---
+++ BadgerVsWalrus/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BadgerVsWalrus
+//
+//  Created by Stefan Church on 22/10/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ BadgerVsWalrus/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BadgerVsWalrus
+//
+//  Created by Stefan Church on 22/10/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
